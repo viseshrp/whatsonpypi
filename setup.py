368b99e2e00d522ca88ecae886ad9bb8101f36d4
viseshrp <viseshrprasad@gmail.com>
2023-01-07 11:19:36 -0500

    make 3.7 the minimum version

--- setup.py
+++ setup.py
@@ -1,66 +1,72 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """The setup script."""
-import io
+import os
+from io import open
 
 from setuptools import setup, find_packages
 
-# Package meta-data.
-NAME = 'whatsonpypi'
-VERSION = '0.2.7'
-DESCRIPTION = "CLI tool to get package info from PyPI and/or manipulate requirements."
-AUTHOR = "Visesh Prasad"
-EMAIL = 'viseshrprasad@gmail.com'
-URL = 'https://github.com/viseshrp/whatsonpypi'
-REQUIRES_PYTHON = ">=2.7"
-REQUIREMENTS = ['future>=0.15.2', 'Click>=7.0', 'requests>=2.18.0', ]
-SETUP_REQUIREMENTS = ['pytest-runner', ]
-TEST_REQUIREMENTS = ['pytest', ]
+REQUIREMENTS = [
+    "click==8.1.3",
+    "requests==2.28.1"
+]
 
-with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
-    README = readme_file.read()
+curr_dir = os.path.abspath(os.path.dirname(__file__))
 
-with io.open('HISTORY.rst', 'r', encoding='utf-8') as history_file:
-    HISTORY = history_file.read()
+
+def get_file_text(file_name):
+    with open(os.path.join(curr_dir, file_name), "r", encoding="utf-8") as in_file:
+        return in_file.read()
+
+
+_version = {}
+_version_file = os.path.join(curr_dir, "whatsonpypi", "__init__.py")
+with open(_version_file) as fp:
+    exec(fp.read(), _version)
+version = _version["__version__"]
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=README + '\n\n' + HISTORY,
-    keywords='whatsonpypi',
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
-    packages=find_packages(include=['whatsonpypi']),
-    include_package_data=True,
+    name="whatsonpypi",
+    version=version,
+    description="CLI tool to get package info from PyPI and add to requirements.",
+    long_description=get_file_text("README.rst")
+                     + "\n\n"
+                     + get_file_text("CHANGELOG.rst"),
+    long_description_content_type="text/x-rst",
+    author="Visesh Prasad",
+    author_email="visesh@live.com",
+    maintainer="Visesh Prasad",
+    maintainer_email="visesh@live.com",
     license="MIT license",
+    packages=find_packages(include=["whatsonpypi"]),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    url="https://github.com/viseshrp/whatsonpypi",
+    project_urls={
+        "Documentation": "https://github.com/viseshrp/whatsonpypi",
+        "Changelog": "https://github.com/viseshrp/whatsonpypi/blob/main/CHANGELOG.md",
+        "Bug Tracker": "https://github.com/viseshrp/whatsonpypi/issues",
+        "Source Code": "https://github.com/viseshrp/whatsonpypi",
+    },
+    python_requires=">=3.7",
+    keywords="whatsonpypi wopp pypi requirements virtualenv venv",
+    test_suite="tests",
+    tests_require=['pytest', ],
+    install_requires=REQUIREMENTS,
     entry_points={
-        'console_scripts': [
-            'whatsonpypi=whatsonpypi.__main__:main',
+        "console_scripts": [
+            "whatsonpypi=whatsonpypi.__main__:main",
         ],
     },
-    python_requires=REQUIRES_PYTHON,
-    install_requires=REQUIREMENTS,
-    setup_requires=SETUP_REQUIREMENTS,
-    test_suite='tests',
-    tests_require=TEST_REQUIREMENTS,
-    zip_safe=False,
 )
