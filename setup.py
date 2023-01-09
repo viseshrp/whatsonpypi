bb1e9d69b95869208183a9cab268b62ce2c0677b
viseshrp <viseshrprasad@gmail.com>
2023-01-08 19:31:56 -0500

    make versions flexible

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from io import open
 
 from setuptools import setup, find_packages
 
-REQUIREMENTS = ["click==8.1.3", "requests==2.28.1"]
+REQUIREMENTS = ["click>=8.1.0", "requests>=2.28.0"]
 
 curr_dir = os.path.abspath(os.path.dirname(__file__))
 
