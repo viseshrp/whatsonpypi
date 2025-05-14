0b80ced298e3290ca212090a26172bc0c426a4d9
viseshrp <viseshrprasad@gmail.com>
2025-05-14 18:43:13 -0400

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,3 +29,21 @@ def test_version(options):
     result = CliRunner().invoke(cli.main, options)
     assert result.exit_code == 0
     assert __version__ in result.output
+
+
+@pytest.mark.parametrize(
+    "work, option, valid",
+    [
+        (["building workedon"], ["-s"], True),
+        (["studying for the GRE"], ["--last"], True),
+        (["talking to my brother", "@ 3pm 3 years ago"], ["--last"], False),
+    ],
+)
+def test_save_and_fetch_last(work, option, valid, cleanup):
+    # save
+    result = CliRunner().invoke(cli.main, work)
+    assert result.output.startswith("Work saved.")
+    # fetch
+    result = CliRunner().invoke(cli.what, option)
+    assert result.exit_code == 0
+    assert "" in result.output
