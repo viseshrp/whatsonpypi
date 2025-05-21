1fdb6406639fcb73d4f9aec180268a0441df5d0d
viseshrp <viseshrprasad@gmail.com>
2025-05-21 17:28:03 -0400

    fix dunder files

--- whatsonpypi/_version.py
+++ whatsonpypi/_version.py
@@ -1,1 +1,7 @@
-__version__ = "0.0.1.dev20230717003019+dirty"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("whatsonpypi")
+except PackageNotFoundError:  # pragma: no cover
+    # Fallback for local dev or editable installs
+    __version__ = "0.0.0"
