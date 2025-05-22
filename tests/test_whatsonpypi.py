06fd81ce29c8457513be76d0a731c7c07fcf8e4e
viseshrp <viseshrprasad@gmail.com>
2025-05-22 15:17:25 -0400

    Update test_whatsonpypi.py

--- tests/test_whatsonpypi.py
+++ tests/test_whatsonpypi.py
@@ -1,11 +1,1 @@
 import pytest
-
-from whatsonpypi.whatsonpypi import do_stuff
-
-
-@pytest.mark.parametrize(
-    "option",
-    ["test1", "test2"],
-)
-def test_do_stuff(option: str) -> None:
-    assert do_stuff(option) == option
