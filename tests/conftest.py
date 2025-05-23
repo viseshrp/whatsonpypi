b40e6dfea4a56fa4007d5ad3ddede2a0986d3bbb
Visesh Rajendraprasad <viseshrprasad@gmail.com>
2025-05-23 14:11:41 -0400

    cruft update

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,13 @@ from collections.abc import Generator
 import pytest
 
 
+def do_cleanup() -> None:
+    """Perform any necessary cleanup after tests."""
+    # Add your cleanup code here
+    pass
+
+
 @pytest.fixture(scope="session", autouse=True)
 def cleanup() -> Generator[None, None, None]:
     yield
-    print("cleanup")
+    do_cleanup()
